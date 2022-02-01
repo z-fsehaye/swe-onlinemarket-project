4b49f98df82b9bde88be5201cc88bf6252548359
z-fsehaye <zfsehaye@miu.edu>
2022-01-31 20:25:50 -0600

    added user class

---
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/User.java
@@ -0,0 +1,19 @@
+package edu.miu.cs.cs425.sweonlinemarketproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+}
