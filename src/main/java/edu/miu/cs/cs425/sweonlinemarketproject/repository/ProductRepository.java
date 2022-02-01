3e7b69257358574527d327c2e01453a16eee6368
z-fsehaye <zfsehaye@miu.edu>
2022-02-01 17:43:03 -0600

    fixed ProductRepository query

--- src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/repository/ProductRepository.java
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package edu.miu.cs.cs425.sweonlinemarketproject.repository;
 import edu.miu.cs.cs425.sweonlinemarketproject.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,8 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "select p from Product p where p.seller.userId = :id")
-    List<Product> findBySellerId(long id);
+    @Query(value = "select p from Product p where p.seller.Id = :id")
+    List<Product> findBySellerId(@Param("id") long id);
 
     @Query(value = "select p from Product p where p.quantity > 0")
     List<Product> findNotOutOfStockProducts();
