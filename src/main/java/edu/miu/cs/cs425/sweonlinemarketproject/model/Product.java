aeb9ce157caa6cec41b836fde03c0af4e0e21880
z-fsehaye <zfsehaye@miu.edu>
2022-01-31 23:50:31 -0600

    added product model

---
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/Product.java
@@ -0,0 +1,41 @@
+package edu.miu.cs.cs425.sweonlinemarketproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+
+    private String title;
+
+    private double quantity;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    private String sellerId;
+
+    private long createdDate;
+
+    private long modifiedDate;
+
+}
