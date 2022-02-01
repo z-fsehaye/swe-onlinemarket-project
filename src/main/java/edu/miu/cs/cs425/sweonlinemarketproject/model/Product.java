258d49d5ed6c45c87a6c5ff2879c4a94a814a553
z-fsehaye <zfsehaye@miu.edu>
2022-02-01 16:20:30 -0600

    updated the validation of product

--- src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/Product.java
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/Product.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
@@ -20,24 +22,33 @@ public class Product implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long productId;
 
+    @NotNull
+    @NotBlank
     @Column(length = 50, nullable = false)
     private String name;
 
+    @NotNull
+    @NotBlank
     @Column(nullable = false)
     private BigDecimal price;
 
+    @NotNull
+    @NotBlank
     @Column(nullable = false)
     private String description;
 
+    @NotNull
+    @NotBlank
     @Column(nullable = false)
-    private double quantity;
+    private int quantity;
 
     private String category;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private List<Review> reviews;
 
-    @OneToOne
+    @ManyToOne
+    @JoinColumn(name = "seller_id", nullable = false)
     private User seller;
 
 }
