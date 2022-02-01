f707d9a81155b61a00c431c58932aafbb96e5303
Amannegash <84345555+Amannegash@users.noreply.github.com>
2022-02-01 17:19:15 -0600

    updated user model

--- src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/User.java
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/User.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,8 +16,40 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;
+    private long Id;
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String userName;
+    @Email
+    private String email;
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+    @NotNull
+    @NotBlank
+//    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    private boolean approvedSeller;
+    private long createDate;
+    private long modifiedDate;
+    @OneToMany(mappedBy = "user")
+    private List<Address> address;
+    @OneToMany(mappedBy = "user")
+    List<Payment> payment;
+
 }
