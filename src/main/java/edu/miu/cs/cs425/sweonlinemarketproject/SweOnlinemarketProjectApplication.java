f53c4c8bfccbb92dc70d141ad5415ad3f8e119d1
z-fsehaye <zfsehaye@miu.edu>
2022-01-31 20:21:33 -0600

    initiated online market project

---
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/SweOnlinemarketProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.sweonlinemarketproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweOnlinemarketProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweOnlinemarketProjectApplication.class, args);
+    }
+
+}
