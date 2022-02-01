017c7200755172477c6f3577f2cee66e4e4e89f7
MusieYemane <m80007278@gmail.com>
2022-02-01 15:11:08 -0600

    added full Order CRUD api

--- src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/controller/OrderController.java
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/controller/OrderController.java
@@ -1,4 +1,39 @@
 package edu.miu.cs.cs425.sweonlinemarketproject.controller;
 
+import edu.miu.cs.cs425.sweonlinemarketproject.model.Order;
+import edu.miu.cs.cs425.sweonlinemarketproject.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/order")
 public class OrderController {
+
+    private OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping(value="/list")
+    public List<Order> getAllOrders(){
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping(value="/{id}")
+    public Order getOrderById(@PathVariable Long id){
+        return orderService.getOrderById(id);
+    }
+
+    @PostMapping(value="/save")
+    public Order saveOrder(@RequestBody Order order){
+        return orderService.saveOrder(order);
+    }
+
+    @DeleteMapping(value="/delete/{id}")
+    public void deleteOrder(@PathVariable Long id){
+        orderService.deleteOrderById(id);
+    }
+
 }
