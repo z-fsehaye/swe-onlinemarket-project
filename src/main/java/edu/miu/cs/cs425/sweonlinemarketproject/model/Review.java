ca25e00551c96ff48a0d1feb227a7eb5e56e561f
z-fsehaye <zfsehaye@miu.edu>
2022-02-01 16:20:50 -0600

    updated the validation of review

--- src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/Review.java
+++ src/main/java/edu/miu/cs/cs425/sweonlinemarketproject/model/Review.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -17,15 +19,19 @@ public class Review implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
     private String comment;
 
     @Column(name = "is_approved")
-    private boolean approveStatus;
+    private boolean isApproved;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
